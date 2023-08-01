44098ece7ddb749dd1e7028e37b8b949ff25a47b
Anthony Estrada <azestrada@outlook.com>
2023-08-01 14:07:14 +0000

    added db types

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,29 +1,29 @@
-
-import type { NextRequest } from 'next/server'
-
-
 import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs'
 import { NextResponse } from 'next/server'
 
-export  function middleware(req:NextRequest) {
- // console.log("middleware working")
+import type { NextRequest } from 'next/server'
+
+export async function middleware(req: NextRequest) {
   const res = NextResponse.next()
   const supabase = createMiddlewareClient({ req, res })
-  const a = supabase.auth.getSession()
- 
-  //const { error } = await supabase.auth.signOut()
-  //const { data: { user } } = await supabase.auth.getUser()
 
-  //const { data: { session } } = await supabase.auth.getSession();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser()
 
- // request: NextRequest
-  //if (request.nextUrl.pathname.startsWith('/about')) {
-   // return NextResponse.rewrite(new URL('/tools/coverletter', request.url))
-  //}
- 
-  ////if (request.nextUrl.pathname.startsWith('/dashboard')) {
-  //  return NextResponse.rewrite(new URL('/dashboard/user', request.url))
-  //}
+  // if user is signed in and the current path is / redirect the user to /account
+  if (user && req.nextUrl.pathname === '/') {
+    return NextResponse.redirect(new URL('/account', req.url))
+  }
+
+  // if user is not signed in and the current path is not / redirect the user to /
+  if (!user && req.nextUrl.pathname !== '/') {
+    return NextResponse.redirect(new URL('/', req.url))
+  }
 
   return res
+}
+
+export const config = {
+  matcher: ['/', '/account'],
 }
---
+++ types/supabase.ts
@@ -0,0 +1,285 @@
+export type Json =
+  | string
+  | number
+  | boolean
+  | null
+  | { [key: string]: Json | undefined }
+  | Json[]
+
+export interface Database {
+  public: {
+    Tables: {
+      config: {
+        Row: {
+          config: Json | null
+          created_at: string | null
+          id: number
+          modified_at: string
+          name: string
+          user_id: number
+        }
+        Insert: {
+          config?: Json | null
+          created_at?: string | null
+          id?: number
+          modified_at?: string
+          name: string
+          user_id: number
+        }
+        Update: {
+          config?: Json | null
+          created_at?: string | null
+          id?: number
+          modified_at?: string
+          name?: string
+          user_id?: number
+        }
+        Relationships: [
+          {
+            foreignKeyName: "config_user_id_fkey"
+            columns: ["user_id"]
+            referencedRelation: "user_info"
+            referencedColumns: ["id"]
+          }
+        ]
+      }
+      coverletter: {
+        Row: {
+          created_at: string | null
+          id: number
+          modified_at: string | null
+          name: string | null
+          user_id: string | null
+        }
+        Insert: {
+          created_at?: string | null
+          id?: number
+          modified_at?: string | null
+          name?: string | null
+          user_id?: string | null
+        }
+        Update: {
+          created_at?: string | null
+          id?: number
+          modified_at?: string | null
+          name?: string | null
+          user_id?: string | null
+        }
+        Relationships: []
+      }
+      jobdetails: {
+        Row: {
+          applied: boolean | null
+          created_at: string | null
+          generatedCoverLetter: string | null
+          id: number
+          job_id: number | null
+          modified_at: string | null
+          status: string | null
+        }
+        Insert: {
+          applied?: boolean | null
+          created_at?: string | null
+          generatedCoverLetter?: string | null
+          id?: number
+          job_id?: number | null
+          modified_at?: string | null
+          status?: string | null
+        }
+        Update: {
+          applied?: boolean | null
+          created_at?: string | null
+          generatedCoverLetter?: string | null
+          id?: number
+          job_id?: number | null
+          modified_at?: string | null
+          status?: string | null
+        }
+        Relationships: [
+          {
+            foreignKeyName: "jobdetails_job_id_fkey"
+            columns: ["job_id"]
+            referencedRelation: "jobs_masterlist"
+            referencedColumns: ["id"]
+          }
+        ]
+      }
+      jobs: {
+        Row: {
+          appliedJobCount: number | null
+          coverletter_id: number | null
+          created_at: string | null
+          email_sending_schedule: string | null
+          id: number
+          jobCount: number | null
+          keyword: string | null
+          modified_at: string | null
+          name: string
+          resume_id: number | null
+          search_texts: Json | null
+          setup_id: number | null
+          status: string | null
+        }
+        Insert: {
+          appliedJobCount?: number | null
+          coverletter_id?: number | null
+          created_at?: string | null
+          email_sending_schedule?: string | null
+          id?: number
+          jobCount?: number | null
+          keyword?: string | null
+          modified_at?: string | null
+          name: string
+          resume_id?: number | null
+          search_texts?: Json | null
+          setup_id?: number | null
+          status?: string | null
+        }
+        Update: {
+          appliedJobCount?: number | null
+          coverletter_id?: number | null
+          created_at?: string | null
+          email_sending_schedule?: string | null
+          id?: number
+          jobCount?: number | null
+          keyword?: string | null
+          modified_at?: string | null
+          name?: string
+          resume_id?: number | null
+          search_texts?: Json | null
+          setup_id?: number | null
+          status?: string | null
+        }
+        Relationships: []
+      }
+      jobs_masterlist: {
+        Row: {
+          created_at: string | null
+          details: Json | null
+          details_version: string | null
+          id: number
+          jobid: string | null
+          jobname: string | null
+          jobverified: boolean | null
+          source: string | null
+          source_site: string | null
+          tags: string | null
+          url: string | null
+        }
+        Insert: {
+          created_at?: string | null
+          details?: Json | null
+          details_version?: string | null
+          id?: number
+          jobid?: string | null
+          jobname?: string | null
+          jobverified?: boolean | null
+          source?: string | null
+          source_site?: string | null
+          tags?: string | null
+          url?: string | null
+        }
+        Update: {
+          created_at?: string | null
+          details?: Json | null
+          details_version?: string | null
+          id?: number
+          jobid?: string | null
+          jobname?: string | null
+          jobverified?: boolean | null
+          source?: string | null
+          source_site?: string | null
+          tags?: string | null
+          url?: string | null
+        }
+        Relationships: []
+      }
+      resume: {
+        Row: {
+          created_at: string | null
+          filepath: string
+          id: number
+          name: string
+          user_id: number
+        }
+        Insert: {
+          created_at?: string | null
+          filepath: string
+          id?: number
+          name: string
+          user_id: number
+        }
+        Update: {
+          created_at?: string | null
+          filepath?: string
+          id?: number
+          name?: string
+          user_id?: number
+        }
+        Relationships: [
+          {
+            foreignKeyName: "resume_user_id_fkey"
+            columns: ["user_id"]
+            referencedRelation: "user_info"
+            referencedColumns: ["id"]
+          }
+        ]
+      }
+      user_info: {
+        Row: {
+          active_subscription: boolean | null
+          active_subscription_id: number | null
+          created_at: string | null
+          email: string
+          fullname: string
+          id: number
+          is_active: boolean | null
+          mobile_no: string | null
+          userid: string
+        }
+        Insert: {
+          active_subscription?: boolean | null
+          active_subscription_id?: number | null
+          created_at?: string | null
+          email: string
+          fullname: string
+          id?: number
+          is_active?: boolean | null
+          mobile_no?: string | null
+          userid: string
+        }
+        Update: {
+          active_subscription?: boolean | null
+          active_subscription_id?: number | null
+          created_at?: string | null
+          email?: string
+          fullname?: string
+          id?: number
+          is_active?: boolean | null
+          mobile_no?: string | null
+          userid?: string
+        }
+        Relationships: [
+          {
+            foreignKeyName: "user_info_userid_fkey"
+            columns: ["userid"]
+            referencedRelation: "users"
+            referencedColumns: ["id"]
+          }
+        ]
+      }
+    }
+    Views: {
+      [_ in never]: never
+    }
+    Functions: {
+      [_ in never]: never
+    }
+    Enums: {
+      [_ in never]: never
+    }
+    CompositeTypes: {
+      [_ in never]: never
+    }
+  }
+}
