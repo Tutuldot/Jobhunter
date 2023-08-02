d95ae5a87992fedc755754a0b76c46c755b23048
Anthony Estrada <azestrada@outlook.com>
2023-08-02 06:59:24 +0000

    added WYSIWYG

--- src/app/(DashboardLayout)/tools/coverletter/add/page.tsx
+++ src/app/(DashboardLayout)/tools/coverletter/add/page.tsx
@@ -4,20 +4,19 @@ import PageContainer from '@/app/(DashboardLayout)/components/container/PageCont
 import DashboardCard from '@/app/(DashboardLayout)/components/shared/DashboardCard';
 import { createClientComponentClient, Session } from '@supabase/auth-helpers-nextjs';
 import { Database } from '../../../../../../types/supabase';
-
+import Editor from 'react-simple-wysiwyg';
 import { useCallback, useEffect, useState } from 'react'
 async function getUser(){
 
 }
 
 export default function AddCoverLetter({ session }: { session: Session | null }) {
-  
+  const [html, setHtml] = useState('my <b>HTML</b>')
   const [cname, setCName] = useState<string | null>(null)
   const [cletter, setCoverletter] = useState<string | null>(null)
   
   async function saveCL(){
     console.log("save records")
-    
 
     const supabase = createClientComponentClient<Database>();
 
@@ -27,7 +26,7 @@ export default function AddCoverLetter({ session }: { session: Session | null })
   
     const { error } = await supabase
       .from('coverletter')
-      .insert({ name: cname,  coverletter: cletter, user_id: user?.id })
+      .insert({ name: cname,  coverletter: html, user_id: user?.id })
 
     console.log(error);
   }
@@ -53,17 +52,8 @@ export default function AddCoverLetter({ session }: { session: Session | null })
              
            />
             <h4>Letter</h4>
-           <TextField 
-              
-              value={cletter || ''}
-              onChange={ (e) => { setCoverletter(e.target.value) }}
-              required
-              
-           
-            
-              fullWidth
-              
-           />
+            <Editor value={html} onChange={ (e) => { setHtml(e.target.value) }} />
+          
            <br></br> <br></br> <br></br>
            <Button variant="outlined" color="secondary" 
            onClick={() => saveCL()}
