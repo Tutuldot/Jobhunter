44b7beb89a370465329c0b970924cf90d006b78d
Anthony Estrada <azestrada@outlook.com>
2023-08-07 04:11:16 +0000

    completed add job

--- src/app/(DashboardLayout)/tools/jobtasks/add/page.tsx
+++ src/app/(DashboardLayout)/tools/jobtasks/add/page.tsx
@@ -1,35 +1,110 @@
 'use client';
-import { Typography, Grid, CardContent,TextField, MenuItem, Button, InputLabel, Select  } from '@mui/material';
+import { Checkbox,TextField, MenuItem, Button, InputLabel, Select  } from '@mui/material';
 import PageContainer from '@/app/(DashboardLayout)/components/container/PageContainer';
 import DashboardCard from '@/app/(DashboardLayout)/components/shared/DashboardCard';
 import { createClientComponentClient, Session } from '@supabase/auth-helpers-nextjs';
 import { Database } from '../../../../../../types/supabase';
 
-import { useState } from 'react'
+import { useState, useCallback, useEffect } from 'react'
 import { useRouter,redirect } from "next/navigation";
 
-
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+import { TimePicker } from '@mui/x-date-pickers';
 export default function AddJobTask({ session }: { session: Session | null }) {
- 
+  const [loading, setLoading] = useState(true)
   const [cname, setCName] = useState<string | null>(null)
   const [resume, setResume] = useState<number>(0)
+  const [cuser, setCUser] = useState(null)
   const [cl, setCl] = useState<number>(0)
+  const [clList, setCllist] = useState(null)
   const [searchStr, setSearchStr] = useState<string | null>(null)
   const [startTime, setStartTime] = useState<string | null>(null)
   const router = useRouter();
-  
-  async function saveCL(){
-    console.log("save records")
+  const supabase = createClientComponentClient<Database>();
+  const [rList, setRlist] = useState(null)
+  const getCoverletter = useCallback(async () => {
+    try {
+      setLoading(true)
+      const {
+        data: { user },
+      } = await supabase.auth.getUser()
+      let { data, error, status } = await supabase
+        .from('coverletter')
+        .select()
+        .eq('user_id', user?.id)
+        .eq('status','Active')
 
-    const supabase = createClientComponentClient<Database>();
+
+      if (error && status !== 406) {
+        throw error
+      }
+
+      if (data) {
+        setCllist(data)
+        setCUser(user)
+        console.log(data)
+      }
+    } catch (error) {
+      alert('Error loading user and coverletter data!')
+    } finally {
+      setLoading(false)
+    }
+  }, [supabase])
+
+  useEffect(() => {
+    getCoverletter()
+  }, [getCoverletter])
+
+
+  // get resume list
+
+  const getResume = useCallback(async () => {
+    try {
+      setLoading(true)
+      const {
+        data: { user },
+      } = await supabase.auth.getUser()
+      let { data, error, status } = await supabase
+        .from('resume')
+        .select()
+        .eq('user_id', user?.id)
+        .is('isdeleted',null)
+     
+
+
+      if (error && status !== 406) {
+        throw error
+      }
+
+      if (data) {
+        setRlist(data)
+        setCUser(user)
+        console.log(data)
+      }
+    } catch (error) {
+      alert('Error loading user and resume data!')
+    } finally {
+      setLoading(false)
+    }
+  }, [supabase])
+
+  useEffect(() => {
+    getResume()
+  }, [getResume])
+
+  
+  async function saveJob(){
+    console.log("saving records")
+
 
     const {
       data: { user },
     } = await supabase.auth.getUser()
   
     const { error } = await supabase
-      .from('resume')
-      .insert({ name: cname, user_id: user?.id })
+      .from('jobs')
+      .insert({ name: cname || '', user_id: user?.id, resume_id: resume, coverletter_id: cl, keyword: searchStr, email_sending_schedule: startTime, status: "Active" })
     if(!error){
       router.replace("/tools/jobtasks")
     }
@@ -43,7 +118,7 @@ export default function AddJobTask({ session }: { session: Session | null }) {
       <DashboardCard title="Create Job Task">
   
         <form autoComplete="off" >
-          
+        <InputLabel >Job Name</InputLabel>
           <TextField 
               label="Name"
               value={cname || ''}
@@ -65,13 +140,18 @@ export default function AddJobTask({ session }: { session: Session | null }) {
               onChange={(e) =>  {setResume(parseInt(e.target.value.toString(),10))} }
             >
                <MenuItem value={0}>Select Resume</MenuItem>
-              <MenuItem value={10}>Ten</MenuItem>
-              <MenuItem value={20}>Twenty</MenuItem>
-              <MenuItem value={30}>Thirty</MenuItem>
+               {rList?.map((resumeList) => (
+                  <MenuItem value={resumeList.id}>{resumeList.name}</MenuItem>
+                ))}
             </Select>
             <br></br> <br></br>
+             
+          <InputLabel >Use AI Cover Letter</InputLabel>
+          <Checkbox disabled /> (Coming Soon)
+          <br></br> <br></br>
             <InputLabel id="cl-label">Cover Letter</InputLabel>
             <Select
+            
               labelId="cl-label"
               id="cl-select"
               value={cl}
@@ -79,14 +159,16 @@ export default function AddJobTask({ session }: { session: Session | null }) {
               onChange={(e) =>  {setCl(parseInt(e.target.value.toString(),10))} }
             >
                <MenuItem value={0}>Select Cover Letter</MenuItem>
-              <MenuItem value={10}>Ten</MenuItem>
-              <MenuItem value={20}>Twenty</MenuItem>
-              <MenuItem value={30}>Thirty</MenuItem>
+               {clList?.map((covLetter) => (
+                  <MenuItem value={covLetter.id}>{covLetter.name}</MenuItem>
+                ))}
+            
             </Select>
            
            <br></br><br></br>
-
+           <InputLabel id="sstring-label">Search String</InputLabel>
            <TextField 
+              
               label="Search String"
               value={searchStr || ''}
               onChange={ (e) => {setSearchStr(e.target.value)} }
@@ -98,11 +180,18 @@ export default function AddJobTask({ session }: { session: Session | null }) {
               fullWidth
              
            />
-          
+          <br></br><br></br>
+          <InputLabel >Send via email ASAP</InputLabel>
+          <Checkbox  />
+          <br></br><br></br>
+          <InputLabel >Sending Schedule</InputLabel>
+          <LocalizationProvider dateAdapter={AdapterDayjs} >
+          <TimePicker label="Select Time" onChange={(value) => {setStartTime(value)}} />
+          </LocalizationProvider>
+        
            <br></br> <br></br> <br></br>
            <Button variant="outlined" color="secondary" 
-           onClick={() => saveCL()}
-           >Save</Button>
+           onClick={() => saveJob()}>Save</Button>
        
        </form>
       </DashboardCard>
