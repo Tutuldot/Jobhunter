e55dc88601cfb9c94d819d5fd6725447586ef01c
Anthony Estrada <azestrada@outlook.com>
2023-08-10 08:49:55 +0000

    job list

--- src/app/(DashboardLayout)/tools/jobtasks/view/[id]/page.tsx
+++ src/app/(DashboardLayout)/tools/jobtasks/view/[id]/page.tsx
@@ -26,6 +26,7 @@ import {
     const [loading, setLoading] = useState(true)
     const [cuser, setCUser] = useState(null)
     const [jdetails, setJDetails] = useState(null)
+    const [jdlines, setJDLines] = useState(null)
     const supabase = createClientComponentClient<Database>();
 
     const getJobTasks = useCallback(async () => {
@@ -36,7 +37,7 @@ import {
           } = await supabase.auth.getUser()
           let { data, error, status } = await supabase
             .from('jobs')
-            .select()
+            .select('*,jobdetails(id, created_at, modified_at, generatedCoverLetter, status, job_header, job_id, jobs_masterlist(source,source_site, url,details, jobid, jobname,jobverified,tags))')
             .eq('user_id', user?.id)
             .eq('id',params.id)
          
@@ -74,9 +75,9 @@ import {
             .select('*,jobdetails(id, created_at, modified_at, generatedCoverLetter, status, job_header, job_id, jobs_masterlist(source,source_site, url,details, jobid, jobname,jobverified,tags))')
             .eq('user_id', user?.id)
             .eq('id',params.id)
+           
+
           
-            console.log('j details')
-            console.log(data)
     
           if (error && status !== 406) {
             throw error
@@ -84,7 +85,10 @@ import {
     
           if (data) {
             setJDetails(data)
-          
+            
+            console.log('job details')
+            console.log(data)
+            data?.map((p) => ( setJDLines(p.jobdetails)))
           }
         } catch (error) {
           console.log('Error loading user and job details data!')
@@ -129,8 +133,218 @@ import {
     
   return (
     <PageContainer title="Job Tasks" description="This page is ">
-      
+       <DashboardCard title="Job Tasks">
 
+     
+<Box sx={{ overflow: 'auto', width: { xs: '280px', sm: 'auto' } }}>
+          <Table
+              aria-label="simple table"
+              sx={{
+                  whiteSpace: "nowrap",
+                  mt: 2
+              }}
+          >
+              <TableHead>
+                  <TableRow>
+                     
+                      <TableCell>
+                          <Typography variant="subtitle2" fontWeight={600}>
+                              Name
+                          </Typography>
+                      </TableCell>
+                      <TableCell>
+                          <Typography variant="subtitle2" fontWeight={600}>
+                              Status
+                          </Typography>
+                      </TableCell>
+                      <TableCell>
+                          <Typography variant="subtitle2" fontWeight={600}>
+                              Job Metrics
+                          </Typography>
+                      </TableCell>
+                      <TableCell align="right">
+                          <Typography variant="subtitle2" fontWeight={600}>
+                              Actions
+                          </Typography>
+                      </TableCell>
+                  </TableRow>
+              </TableHead>
+              <TableBody>
+                 
+
+                  {clList?.map((product) => (
+                      <TableRow key={product.id}>
+                         
+                          <TableCell>
+                              <Box
+                                  sx={{
+                                      display: "flex",
+                                      alignItems: "center",
+                                  }}
+                              >
+                                  <Box>
+                                      <Typography variant="subtitle2" fontWeight={600}>
+                                          {product.name}
+                                      </Typography>
+                                     
+                                  </Box>
+                              </Box>
+                          </TableCell>
+                          
+                         
+                          <TableCell>
+                              <Chip
+                                  sx={{
+                                      px: "4px",
+                                      backgroundColor: (product.status == "Active") ? "green" : "Red",
+                                      color: "#fff",
+                                  }}
+                                  size="small"
+                                  label={product.status}
+                              ></Chip>
+                          </TableCell>
+
+
+                          <TableCell>
+                              <Box
+                                  sx={{ display: "flex", alignItems: "center",}}
+                              >
+                                  <Box>
+                                      <Typography variant="subtitle2" fontWeight={600}>
+                                      {product.appliedJobCount || 0}  / {product.jobCount || 0} ({(((product.appliedJobCount || 0) / (product.jobCount || 1)) * 100).toFixed(0)} %)
+                                      </Typography>
+                                     
+                                  </Box>
+                              </Box>
+                          </TableCell>
+
+                          <TableCell align="right">
+                          <IconButton color="primary" aria-label="Delete"
+                          onClick={() => deactivateJobs( product.id.toString())}
+                           >
+                               <IconTrash />
+                          </IconButton>
+                          <IconButton color="primary" aria-label="Download" href={"/tools/jobtasks/edit/"  + product.id.toString()} component={Link}>
+                               <IconEdit />
+                          </IconButton>
+
+                        
+                          </TableCell>
+                      </TableRow>
+                  ))}
+              </TableBody>
+          </Table>
+      </Box>
+</DashboardCard>
+
+<br/><br/>
+
+
+<DashboardCard title="Job Items">
+
+     
+<Box sx={{ overflow: 'auto', width: { xs: '280px', sm: 'auto' } }}>
+          <Table
+              aria-label="simple table"
+              sx={{
+                  whiteSpace: "nowrap",
+                  mt: 2
+              }}
+          >
+              <TableHead>
+                  <TableRow>
+                     
+                      <TableCell>
+                          <Typography variant="subtitle2" fontWeight={600}>
+                              Name
+                          </Typography>
+                      </TableCell>
+                      <TableCell>
+                          <Typography variant="subtitle2" fontWeight={600}>
+                              Status
+                          </Typography>
+                      </TableCell>
+                      <TableCell>
+                          <Typography variant="subtitle2" fontWeight={600}>
+                              Job Metrics
+                          </Typography>
+                      </TableCell>
+                      <TableCell align="right">
+                          <Typography variant="subtitle2" fontWeight={600}>
+                              Actions
+                          </Typography>
+                      </TableCell>
+                  </TableRow>
+              </TableHead>
+              <TableBody>
+                 
+
+                  {jdlines?.map((product) => (
+                      <TableRow key={product.id}>
+                         
+                          <TableCell>
+                              <Box
+                                  sx={{
+                                      display: "flex",
+                                      alignItems: "center",
+                                  }}
+                              >
+                                  <Box>
+                                      <Typography variant="subtitle2" fontWeight={600}>
+                                          {product.jobs_masterlist.details.jobTitle} <br/>
+                                          {product.jobs_masterlist.details.source} 
+                                      </Typography>
+                                     
+                                  </Box>
+                              </Box>
+                          </TableCell>
+                          
+                         
+                          <TableCell>
+                              <Chip
+                                  sx={{
+                                      px: "4px",
+                                      backgroundColor: (product.status == "Active") ? "green" : "Red",
+                                      color: "#fff",
+                                  }}
+                                  size="small"
+                                  label={product.status}
+                              ></Chip>
+                          </TableCell>
+
+
+                          <TableCell>
+                              <Box
+                                  sx={{ display: "flex", alignItems: "center",}}
+                              >
+                                  <Box>
+                                      <Typography variant="subtitle2" fontWeight={600}>
+                                     0
+                                      </Typography>
+                                     
+                                  </Box>
+                              </Box>
+                          </TableCell>
+
+                          <TableCell align="right">
+                          <IconButton color="primary" aria-label="Delete"
+                          onClick={() => deactivateJobs( product.id.toString())}
+                           >
+                               <IconTrash />
+                          </IconButton>
+                          <IconButton color="primary" aria-label="Download" href={"/tools/jobtasks/edit/"  + product.id.toString()} component={Link}>
+                               <IconEye />
+                          </IconButton>
+
+                        
+                          </TableCell>
+                      </TableRow>
+                  ))}
+              </TableBody>
+          </Table>
+      </Box>
+</DashboardCard>
+      
 
     </PageContainer>
   );
